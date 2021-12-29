08e0d9472aee05884dd8315b1b10487883ba0274
Alex <tan.08.07.1997@gmail.com>
2021-12-29 07:49:52 +0700

    init project

---
+++ src/main/java/com/code4life/simplewebapplication/SimpleController.java
@@ -0,0 +1,13 @@
+package com.code4life.simplewebapplication;
+
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @RequestMapping("/")
+    public String index(){
+        return "hello";
+    }
+}
